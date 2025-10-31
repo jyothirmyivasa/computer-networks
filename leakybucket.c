7f63335b0dd8107d1e84f14362910908d93320a9
jyothirmyivasa <151708381+jyothirmyivasa@users.noreply.github.com>
2025-10-31 11:31:29 +0530

    leakybucket.c

--- leakybucket.c
+++ leakybucket.c
@@ -42,3 +42,20 @@ void main(){
         }
         printf("\nAll packets transmitted successfully!\n");
 }
+o/p:
+=== Leaky Bucket Algorithm Simulation ===
+Enter bucket capacity (in packets): 3
+Enter output rate(packets per second): 2
+Enter number of incoming packets (time slots): 3
+Time 1: Enter number of incoming packets: 2
+Incoming Packets: 2
+Transmitted: 2 |Packets left in bucket: 0
+Time 2: Enter number of incoming packets: 3
+Incoming Packets: 3
+Transmitted: 2 | Packets left in bucket:1
+Time 3: Enter number of incoming packets: 3
+Incoming Packets: 3
+Bucket overflow! Dropped packets: 1
+Transmitted: 2 | Packets left in bucket: 1
+Transmitted: 1 |packets left in bucket : 0
+All packets transmitted successfully!
